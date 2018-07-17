fc3098ed0e71e40034fa00774fff98cedaff1dfc
luan ferreira <luan1988@gmail.com>
2018-07-17 20:48:40 -0300

    ADD MSG XIMIRA XONGAO

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("XELO XIMIRA XONGAO!");           
+
+            Console.ReadLine();
+        }
+    }
+}
